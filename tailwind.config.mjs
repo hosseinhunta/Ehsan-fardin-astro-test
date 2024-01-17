8974341a1014b1b5b8f3539566e7bc97ba830f04
Hossein Mohmmadian <hosseinhunta@hotmail.com>
2024-01-17 18:55:38 +0330

    Fardin Ez Astro Test Project

---
+++ tailwind.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: false,
+  variants: {
+    extend: {
+      scale: ["group-hover"],
+    },
+  },
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1500px",
+      // => @media (min-width: 1400px) { ... }
+    },
+  },
+  plugins: [],
+};
